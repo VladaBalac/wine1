9ac489e4acc0b046abb254148a200272273450ee
Vladimir Balac <balacvladimir8@gmail.com>
2019-04-06 16:26:26 +0200

    Start the project

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './core/header/header.component';
+import { NavbarComponent } from './core/navbar/navbar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    NavbarComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
