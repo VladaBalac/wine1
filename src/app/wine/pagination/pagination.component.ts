a0c47577bbedeacc298c2a391fde024e99c0abbb
Vladimir Balac <balacvladimir8@gmail.com>
2019-04-09 00:33:46 +0200

    Pagination

--- src/app/wine/pagination/pagination.component.ts
+++ src/app/wine/pagination/pagination.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
-  selector: 'app-pagination',
-  templateUrl: './pagination.component.html',
-  styleUrls: ['./pagination.component.css']
+	selector: 'app-pagination',
+	templateUrl: './pagination.component.html',
+	styleUrls: ['./pagination.component.css']
 })
-export class PaginationComponent implements OnInit {
+export class PaginationComponent implements OnInit, OnChanges {
 
-  constructor() { }
+	@Input()noWine :number;
+	@Input()showPage :number;
+	@Output() givePage :EventEmitter<number>;
+	pagList:number[] = [];
+	activePage:number = 1;
 
-  ngOnInit() {
-  }
+	constructor() {
+		this.givePage = new EventEmitter();
+	}
+
+	private calculate(): number{
+		return Math.ceil(this.noWine/this.showPage);
+	}
+
+	ngOnInit() {
+	}
+
+	ngOnChanges(){
+		for(let i = 1; i <= this.calculate(); i++){
+			this.pagList.push(i);
+		}}
+
+		selectedPage(page: number){
+			if(page >= 1 && page <= this.calculate()){
+				this.activePage = page;
+				this.givePage.emit(this.activePage);
+			}
+		}
+
+	}
+
+
+
+
 
 }
